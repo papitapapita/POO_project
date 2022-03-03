f0d2074a3e46c66a2c50dbe4f414e68502f43e6b
Jonathan <many.d.r.o.2001@hotmail.com>
2022-03-03 14:42:31 -0500

    Constructor de vehiculo hecho

--- main.cpp
+++ main.cpp
@@ -14,16 +14,122 @@ class Vehiculo
 public:
     Vehiculo(float manejo, float frenado, float offroad, float valor, float salida, float aceleracion, float velocidad, float peso, int modelo, int potencia, int torque, string tipo, Marca *marca, Llanta *llantas)
     {
-        manejo, frenado, offroad, valor, salida, aceleracion, velocidad, peso
+
+        this->manejo = manejo;
+        this->frenado = frenado;
+        this->offroad = offroad;
+        this->valor = valor;
+        this->salida = salida;
+        this->aceleracion = aceleracion;
+        this->velocidad = velocidad;
+        this->peso = peso;
+        this->modelo = modelo;
+        this->potencia = potencia;
+        this->torque = torque;
+        this->tipo = tipo;
+        this->marca = marca;
+        this->llantas = llantas;
+    }
+    Vehiculo()
+    {
+        manejo = frenado = offroad = valor = salida = aceleracion = velocidad = peso = modelo = potencia = torque = 0;
+        tipo = " ";
+        marca = {};
+        llantas = {};
     }
 };
 
 class Marca
 {
+    string nombre, pais;
+    int anio_fundacion;
+
+public:
+    Marca()
+    {
+    }
 };
 
 class Llanta
 {
+public:
+    Llanta()
+    {
+    }
+};
+
+class Motor
+{
+    string admision, inyeccion, encendido, escape, arbol_levas, valvulas, cilindrada, pistones, turbo, intercooler, aceite;
+
+public:
+    Motor()
+    {
+        admision = inyeccion = encendido = escape = arbol_levas = valvulas = cilindrada = pistones = turbo = intercooler = aceite = "";
+    }
+    Motor(string admision, string inyeccion, string encendido, string escape, string arbol_levas, string valvulas, string cilindrada, string pistones, string turbo, string intercooler, string aceite)
+    {
+        this->admision = admision;
+        this->inyeccion = inyeccion;
+        this->encendido = encendido;
+        this->escape = escape;
+        this->arbol_levas = arbol_levas;
+        this->valvulas = valvulas;
+        this->cilindrada = cilindrada;
+        this->pistones = pistones;
+        this->turbo = turbo;
+        this->intercooler = intercooler;
+        this->aceite = aceite;
+    }
+    string getAdmision()
+    {
+        return admision;
+    }
+    string getInyeccion()
+    {
+        return inyeccion;
+    }
+    string getEncendido()
+    {
+        return encendido;
+    }
+    string getEscape()
+    {
+        return escape;
+    }
+    string getArbolLevas()
+    {
+        return arbol_levas;
+    }
+    string getValvulas()
+    {
+        return valvulas;
+    }
+    string getCilindrada()
+    {
+        return cilindrada;
+    }
+    string getPistones()
+    {
+        return pistones;
+    }
+    string getTurbo()
+    {
+        return turbo;
+    }
+    string getIntercooler()
+    {
+        return intercooler;
+    }
+    string getAceite()
+    {
+        return aceite;
+    }
+};
+
+class Motorizados
+{
+    Motor *motor;
 };
 
 int main()
