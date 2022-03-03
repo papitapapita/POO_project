cc7242427db474da582259f2361fb05f366349d3
Jonathan <many.d.r.o.2001@hotmail.com>
2022-03-03 15:10:21 -0500

    getters, setters, constructors de Marca

--- main.cpp
+++ main.cpp
@@ -178,8 +178,40 @@ class Marca
     int anio_fundacion;
 
 public:
+    Marca(string nombre, string pais, int anio_fundacion)
+    {
+        this->nombre = nombre;
+        this->pais = pais;
+        this->anio_fundacion = anio_fundacion;
+    }
     Marca()
     {
+        nombre = pais = "";
+        anio_fundacion = 0;
+    }
+    string getNombre()
+    {
+        return nombre;
+    }
+    string getPais()
+    {
+        return pais;
+    }
+    int getAnioFundacion()
+    {
+        return anio_fundacion;
+    }
+    void setNombre(string nombre)
+    {
+        this->nombre = nombre;
+    }
+    void setPais(string pais)
+    {
+        this->pais = pais;
+    }
+    void setAnioFundacion(int anio_fundacion)
+    {
+        this->anio_fundacion = anio_fundacion;
     }
 };
 
