7f95ffc4fbeabdbc3996d77c42a15d3f3cf4c831
Jonathan <many.d.r.o.2001@hotmail.com>
2022-03-11 10:22:15 -0500

    rin class created

---
+++ rin.h
@@ -0,0 +1,40 @@
+#ifndef POO_PROJECT_RIN_H
+#define POO_PROJECT_RIN_H
+#include <iostream>
+using namespace std;
+
+class Rin
+{
+    string rin;
+    float diametroRin;
+
+public:
+    Rin()
+    {
+        rin = "";
+        diametroRin = 0;
+    }
+    Rin(string rin, float diametroRin)
+    {
+        this->rin = rin;
+        this->diametroRin = diametroRin;
+    }
+    string getRin()
+    {
+        return rin;
+    }
+    float getDiametroRin()
+    {
+        return diametroRin;
+    }
+    void setRin(string rin)
+    {
+        this->rin = rin;
+    }
+    void setDiametroRin(float diametroRin)
+    {
+        this->diametroRin = diametroRin;
+    }
+};
+
+#endif
